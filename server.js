2ad6555976ea107bf1ec94ba560f905055cfb933
arthurs14 <arthurcs426@gmail.com>
2023-10-17 14:00:37 -0700

    added helmet to hide which server was used in network tab

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+// IMPORTS
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
 const express = require('express');
+const helmet = require('helmet');
 
+// START OF FILE
 const PORT = 8000;
-
 const app = express();
 
+// MIDDLEWARE
+app.use(helmet());
+
+// ROUTES
 app.get('/secret', (req, res) => {
   return res.send('Your personal secret value is 42!');
 });
@@ -15,6 +21,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// START SERVER
 https
   .createServer(
     {
