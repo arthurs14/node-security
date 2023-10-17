378fb55f667dce7af3be2c3516b00a834dd64eaa
arthurs14 <arthurcs426@gmail.com>
2023-10-17 13:28:57 -0700

    added express and html file

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const path = reuire('path');
 const express = require('express');
+
+const PORT = 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
