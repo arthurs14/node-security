fc23a5eb817b4ef9ba0de4a9a92342c9da793b83
arthurs14 <arthurcs426@gmail.com>
2023-10-19 14:18:35 -0700

    protected route now accessible when logged in

--- server.js
+++ server.js
@@ -60,7 +60,8 @@ app.use(passport.session());
 // --- FUNCTIONS ---
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true; // TODO
+  console.log(`Current user is:`, req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user;
 
   if (!isLoggedIn) {
     return res.status(401).json({ error: 'You must log in!' });
